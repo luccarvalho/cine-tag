f293dddb06565459e36bd5761b37d3c6f80d94d6
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-07-30 11:25:06 -0300

    Adicionando cards a página inicial.

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,9 @@ import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
+import styles from "./Inicio.module.css";
+import Card from "components/Card";
+import videos from "json/db.json";
 
 export default function Inicio() {
   return (
@@ -12,6 +15,11 @@ export default function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
       <Rodape />
     </>
   );
