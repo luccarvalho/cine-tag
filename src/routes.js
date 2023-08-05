639349ece5fc0546d8fed414a707a8bb84164abe
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-08-05 12:00:37 -0300

    Criando rota para página base.

--- src/routes.js
+++ src/routes.js
@@ -2,28 +2,21 @@ import React from "react";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Favoritos from "pages/Favoritos";
-import Cabecalho from "components/Cabecalho";
-import Rodape from "components/Rodape";
-import Container from "components/Container";
-import FavoritosProvider from "contextos/Favoritos";
 import Player from "pages/Player";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Cabecalho />
-      <Container>
-        <FavoritosProvider>
-          <Routes>
-            <Route path="/" element={<Inicio />}></Route>
-            <Route path="/favoritos" element={<Favoritos />}></Route>
-            <Route path="/:id" element={<Player />}></Route>
-            <Route path="*" element={<NaoEncontrada />}></Route>
-          </Routes>
-        </FavoritosProvider>
-      </Container>
-      <Rodape />
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="favoritos" element={<Favoritos />}></Route>
+          <Route path=":id" element={<Player />}></Route>
+          <Route path="*" element={<NaoEncontrada />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
