30fbda9187f866559f5468bc4df6220e31d40906
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-08-05 12:05:00 -0300

    Criação do componente Página Base.

---
+++ src/pages/PaginaBase/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Cabecalho from "components/Cabecalho";
+import FavoritosProvider from "contextos/Favoritos";
+import Container from "components/Container";
+import { Outlet } from "react-router-dom";
+import Rodape from "components/Rodape";
+
+export default function PaginaBase() {
+  return (
+    <main>
+      <Cabecalho />
+      <FavoritosProvider>
+        <Container>
+          <Outlet />
+        </Container>
+      </FavoritosProvider>
+      <Rodape />
+    </main>
+  );
+}
