572efc3b6e2124546e52f49bcf9a753f6747b4ee
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-08-07 16:59:00 -0300

    Criando uma simulação (mockando) de API para os vídeos da página inicial.

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import styles from "./Inicio.module.css";
 import Card from "components/Card";
-import videos from "json/db.json";
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/luccarvalho/cine-tag/videos")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideos(dados);
+      });
+  }, []);
+
   return (
     <>
       <Banner imagem="home" />
